2529dc5f0dc091676f9da5a1f973b39b3bb8d2f9
hankexin <iskexin@163.com>
2017-08-22 15:31:38 +0800

    first

---
+++ dist/js/controlManager.js
@@ -0,0 +1,25 @@
+//管理index索引
+(function($,root){
+	function controlManager(length){
+		this.index = 0;
+		this.length = length;
+	}
+	controlManager.prototype = {
+		//下一首
+		next : function(){
+			return this.getIndex(1);
+		},
+		//上一首
+		prev : function(){
+			return this.getIndex(-1);
+		},
+		getIndex:function(value){
+			var index = this.index;
+			var len = this.length;
+			var curIndex = (index + value + len) % len;
+			this.index = curIndex;
+			return curIndex;
+		}
+	}
+	root.controlManager = controlManager;
+}(window.Zepto,window.player || (window.player = {})))
